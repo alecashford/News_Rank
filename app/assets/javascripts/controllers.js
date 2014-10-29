51ac38059cdfaf17ea6ce4b11cb8685c91a200b9
Alec Ashford <ashford91@gmail.com>
2014-10-29 12:34:51 -0700

    heroku debugging

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,59 +1,59 @@
 app.controller('MainController', ["$scope", "$http", function($scope, $http) {
 
-    $scope.activeTiles = []
+    $scope.activeTiles = [];
 
-    $scope.tiles = []
+    $scope.tiles = [];
 
-    $scope.sortBy = "calculated_rank"
+    $scope.sortBy = "calculated_rank";
 
     $scope.getArticles = function() {
         $http({
             method: 'GET',
             url: '/articles'
         }).success(function(data) {
-            $scope.tiles = []
-            var merged = []
-            merged = merged.concat.apply(merged, data)
+            $scope.tiles = [];
+            var merged = [];
+            merged = merged.concat.apply(merged, data);
             for (i = 0; i < merged.length; i++) {
-                $scope.tiles.push(merged[i])
+                $scope.tiles.push(merged[i]);
             }
-            $scope.initializePage($scope.sortBy)
-            console.log($scope.tiles)
+            $scope.initializePage($scope.sortBy);
+            console.log($scope.tiles);
         })
     }
 
     $scope.initializePage = function(sortBy) {
-        $scope.activeTiles = []
-        sortFeed($scope.tiles, sortBy)
+        $scope.activeTiles = [];
+        sortFeed($scope.tiles, sortBy);
         if ($scope.tiles.length < 18) {
-            var minimumCount = $scope.tiles.length
+            var minimumCount = $scope.tiles.length;
         } else {
-            var minimumCount = 18
+            var minimumCount = 18;
         }
         for (i = 0; i < minimumCount; i++) {
-            $scope.activeTiles.push($scope.tiles[i])
+            $scope.activeTiles.push($scope.tiles[i]);
         }
     }
 
     $scope.makeUsable = function(rawFloat) {
-        return parseInt(rawFloat * 100000000000000000)
+        return parseInt(rawFloat * 100000000000000000);
     }
 
     var sortFeed = function(sortMe, sortBy) {
         var spaceship = function(a, b) {
             if (a[sortBy] < b[sortBy]) {
-                return 1
+                return 1;
             }
             else if (a[sortBy] > b[sortBy]) {
-                return -1
+                return -1;
             }
             else {
-                return 0
+                return 0;
             }
         }
-        $scope.tiles = $scope.tiles.sort(spaceship)
+        $scope.tiles = $scope.tiles.sort(spaceship);
         if (sortBy == "calculated_rank") {
-            $scope.tiles.reverse()
+            $scope.tiles.reverse();
         }
     }
 
@@ -61,150 +61,150 @@ app.controller('MainController', ["$scope", "$http", function($scope, $http) {
     $scope.addFeedFromUrl = function() {
         $http.post('/feeds/create',
         { url: $scope.newFeedUrl })
-        .success($scope.updateUserFeeds())
+        .success($scope.updateUserFeeds());
     }
 
     $scope.loadMoreTiles = function() {
-        var lastTile = $scope.activeTiles.length
+        var lastTile = $scope.activeTiles.length;
         for (i = 0; i < 9; i++) {
             if ($scope.activeTiles.length < $scope.tiles.length) {
-                $scope.activeTiles.push($scope.tiles[i + lastTile])
+                $scope.activeTiles.push($scope.tiles[i + lastTile]);
             }
         }
     }
 
-    $scope.timePublishedButtonActive = false
+    $scope.timePublishedButtonActive = false;
 
-    $scope.newsRankButtonActive = true
+    $scope.newsRankButtonActive = true;
 
     $scope.sortTimePublished = function(){
-        $scope.sortBy = "published"
-        $scope.initializePage($scope.sortBy)
-        $scope.timePublishedButtonActive = true
-        $scope.newsRankButtonActive = false
+        $scope.sortBy = "published";
+        $scope.initializePage($scope.sortBy);
+        $scope.timePublishedButtonActive = true;
+        $scope.newsRankButtonActive = false;
     }
 
     $scope.sortNewsRank= function(){
-        $scope.sortBy = "calculated_rank"
-        $scope.initializePage($scope.sortBy)
-        $scope.timePublishedButtonActive = false
-        $scope.newsRankButtonActive = true
+        $scope.sortBy = "calculated_rank";
+        $scope.initializePage($scope.sortBy);
+        $scope.timePublishedButtonActive = false;
+        $scope.newsRankButtonActive = true;
     }
 
-    $scope.checkedBoxes = []
+    $scope.checkedBoxes = [];
 
     $scope.log = function(){
-        console.log($scope.checkedBoxes)
+        console.log($scope.checkedBoxes);
     }
 
     $scope.toggleResults = function(result){
         var found = $.inArray(result.feedId, $scope.checkedBoxes) > -1;
         if (found) {
-            index = $scope.checkedBoxes.indexOf(result.feedId)
-            $scope.checkedBoxes.splice(index, 1)
-            result.selected=false
+            index = $scope.checkedBoxes.indexOf(result.feedId);
+            $scope.checkedBoxes.splice(index, 1);
+            result.selected = false;
         }
         else{
-            $scope.checkedBoxes.push(result.feedId)
-            result.selected = true
+            $scope.checkedBoxes.push(result.feedId);
+            result.selected = true;
         }
     }
 
-    $scope.searchResults = []
+    $scope.searchResults = [];
 
-    $scope.hideSubscribeButton = true
+    $scope.hideSubscribeButton = true;
 
     $scope.search = function() {
         if ($scope.searchTerm.substring(0, 7) == "http://") {
             $http.post('/feeds/create',
             { url: $scope.searchTerm })
             .success(function() {
-                $scope.resetAll()
-                $scope.updateUserFeeds()
-                $scope.getArticles()
+                $scope.resetAll();
+                $scope.updateUserFeeds();
+                $scope.getArticles();
             })
         }
         else {
             $http.post('/feeds/search',
             { url: $scope.searchTerm })
             .success(function(data) {
-            $scope.searchResults = data
+            $scope.searchResults = data;
             })
-            $scope.hideSubscribeButton = false
+            $scope.hideSubscribeButton = false;
         }
     }
 
-    $scope.addFromSearch = function(){
-        for (i = 0; i < $scope.checkedBoxes.length; i++){
+    $scope.addFromSearch = function() {
+        for (i = 0; i < $scope.checkedBoxes.length; i++) {
             $http.post('/feeds/create',
             {url: $scope.checkedBoxes[i]})
             .success(function() {
-                $scope.resetAll()
-                $scope.updateUserFeeds()
-                $scope.getArticles()
+                $scope.resetAll();
+                $scope.updateUserFeeds();
+                $scope.getArticles();
             })
         }
-        $scope.updateUserFeeds()
+        $scope.updateUserFeeds();
     }
 
     $scope.toHour = function(published) {
-        var age = new Date().getTime() - parseInt(published)
-        var min = parseInt(age/60000)
-        var string = ""
+        var age = new Date().getTime() - parseInt(published);
+        var min = parseInt(age/60000);
+        var string = "";
         if (min < 60) {
-            string = min + "m"
+            string = min + "m";
         }
         else {
-            hours = parseInt(min / 60)
-            minutes = min % 60
+            hours = parseInt(min / 60);
+            minutes = min % 60;
             if (minutes == 0) {
-                string = hours + "h"
+                string = hours + "h";
             }
             else {
-                string = hours + "h " + minutes + "m"
+                string = hours + "h " + minutes + "m";
             }
         }
         if (min > 1440) {
-            string = parseInt(min/1440) + "d"
+            string = parseInt(min/1440) + "d";
         }
-        return string
+        return string;
     }
 
     $scope.imgHelper = function(tile) {
         if (tile.visual_url == null) {
-            return "http://i.imgur.com/JFZ8pp4.jpg"
+            return "http://i.imgur.com/JFZ8pp4.jpg";
         }
         else if (tile.visual_url.substring(0, 7) == "http://") {
-            return tile.visual_url
+            return tile.visual_url;
         }
         else {
-            return "http://i.imgur.com/JFZ8pp4.jpg"
+            return "http://i.imgur.com/JFZ8pp4.jpg";
         }
     }
 
-    $scope.userFeeds = []
+    $scope.userFeeds = [];
 
-    $scope.updateUserFeeds = function(){
+    $scope.updateUserFeeds = function() {
         $http({
             method: 'GET',
             url: '/feeds'
         }).success(function(data) {
-            $scope.userFeeds = []
-            $scope.userFeeds = data
+            $scope.userFeeds = [];
+            $scope.userFeeds = data;
         })
     }
 
-    $scope.deleteFeed = function(feed_id){
-        $http.delete('/feeds/delete/' + feed_id)
-        $scope.updateUserFeeds()
+    $scope.deleteFeed = function(feed_id) {
+        $http.delete('/feeds/delete/' + feed_id);
+        $scope.updateUserFeeds();
     }
 
     $scope.resetAll = function() {
-        $("input[type=text], textarea").val("")
-        $('#fade, .popup:visible').fadeOut('normal', function() { $('#fade, .popup:visible').css('display','none')})
-        $scope.initializePage($scope.sortBy)
-        $scope.searchResults = []
-        $scope.hideSubscribeButton = true
+        $("input[type=text], textarea").val("");
+        $('#fade, .popup:visible').fadeOut('normal', function() { $('#fade, .popup:visible').css('display','none')});
+        $scope.initializePage($scope.sortBy);
+        $scope.searchResults = [];
+        $scope.hideSubscribeButton = true;
     }
 
 }]);
