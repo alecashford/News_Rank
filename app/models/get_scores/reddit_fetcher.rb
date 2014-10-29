aad35bfaefb6c40836e1992a611fbb6baa5eca17
Alec Ashford <ashford91@gmail.com>
2014-10-29 11:26:13 -0700

    heroku debugging

--- app/models/get_scores/reddit_fetcher.rb
+++ app/models/get_scores/reddit_fetcher.rb
@@ -6,13 +6,16 @@ module GetScores
     end
 
     def fetch
+      p "fetching from reddit..."
       response = HTTParty.get("http://www.reddit.com/api/info.json?url=#{@url}")
     rescue
         nil
     end
 
     def scores
+      p "Now in reddit fetcher."
       if fetch
+        P "Fetch is true!"
         score = 0
         num_comments = 0
         fetch["data"]["children"].each do |article|
@@ -20,6 +23,7 @@ module GetScores
           num_comments += article["data"]["num_comments"]
         end
       end
+      p "supposedly about to return the scores..."
       {
         score: score,
         num_comments: num_comments
